9d7c22ff1ddea8a4e1fd7e23ad78eaa459dd81a1
hillabrahams <31187378+hillabrahams@users.noreply.github.com>
2025-06-05 20:55:13 -0400

    new endgrok endpoint

--- lib/screens/journal_analyzer_screen.dart
+++ lib/screens/journal_analyzer_screen.dart
@@ -168,7 +168,7 @@ class _JournalAnalyzerScreenState extends State<JournalAnalyzerScreen> {
   }
 
   Future<Map<String, dynamic>?> analyzeEntry(String entryText) async {
-    const String url = "https://3f31b6c446ea.ngrok.app/analyze";
+    const String url = "https://57eddf67e782.ngrok.app/analyze";
     String currentUrl = url;
     int redirectCount = 0;
     const int maxRedirects = 5;
